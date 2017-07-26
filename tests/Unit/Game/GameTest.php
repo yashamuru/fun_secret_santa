ae88630b8f3f331a2ef528db78b1a352de19570b
Cvetan Nenov <yashamuru@gmail.com>
2017-07-26 17:55:48 +0200

    initial test

---
+++ tests/Unit/Game/GameTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Game;
+
+use PHPUnit\Framework\TestCase;
+
+class GameTest extends TestCase {
+
+    public function testInitial()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
